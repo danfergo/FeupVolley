b47c13d8765b2101d462fb004053b4b48a4e5e2d
danfergo <danfergo@gmail.com>
2016-03-10 14:25:04 +0000

    files

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+        Input.GetAxis("Horizontal");
+        Input.GetAxis("Vertical");
+
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        rb.velocity = new Vector3(-v, 0, h) * speed;
+    }
+}
---
+++ Assets/Scripts/SlowGravity.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlowGravity : MonoBehaviour {
+
+    Rigidbody rb;
+    public float antiGravity;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+            rb.AddForce(Physics.gravity* -1 * antiGravity * rb.mass);
+
+
+    }
+
+    void Update() {
+       // UpdateTrajectory(this.transform.position, this.GetComponent<Rigidbody>().velocity, new Vector3(0, -10, 0));
+
+    }
+
+    void UpdateTrajectory(Vector3 initialPosition, Vector3 initialVelocity, Vector3 gravity)
+    {
+        int numSteps = 100; // for example
+        float timeDelta = 1.0f / initialVelocity.magnitude; // for example
+
+        LineRenderer lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.SetVertexCount(numSteps);
+
+        Vector3 position = initialPosition;
+        Vector3 velocity = initialVelocity;
+        for (int i = 0; i < numSteps; ++i)
+        {
+            lineRenderer.SetPosition(i, position);
+
+            position += velocity * timeDelta + 0.5f * gravity * timeDelta * timeDelta;
+            velocity += gravity * timeDelta;
+        }
+    }
+}
