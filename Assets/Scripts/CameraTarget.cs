bd1b23871e7cb3362a8ef43bb586a73e5402a8cf
Cowtard <ei12109@fe.up.pt>
2016-03-10 22:22:22 +0000

    Camera Testes

---
+++ Assets/Scripts/CameraTarget.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraTarget : MonoBehaviour {
+
+
+    public GameObject target;
+    public GameObject ball;
+
+    public float speed = 10f;
+    float angle = 0f;
+	// Use this for initialization
+	void Start () {
+        transform.LookAt(target.transform.position);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (ball.transform.position.z > target.transform.position.z)
+        {
+            if (transform.position.z >= 15)
+            {
+                angle = 0;
+            }
+            else
+            {
+                angle = -speed;
+            }
+        }
+        else {
+            if (transform.position.z <= 5)
+            {
+                angle = 0;
+            }
+            else
+            {
+                angle = speed;
+            }
+        }
+
+        transform.RotateAround(target.transform.position, new Vector3(0,1f,0), angle / 2);
+
+    }
+}
