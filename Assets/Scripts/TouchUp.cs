7e5844d4a35ecaf482af5bcd0b7213149c866b9c
Diogo Gomes <Diogo Gomes>
2016-03-14 09:45:07 +0000

    Added a simple UI

--- Assets/Scripts/TouchUp.cs
+++ Assets/Scripts/TouchUp.cs
@@ -1,11 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class TouchUp : MonoBehaviour {
 	private Vector3 airTargetPosition;
 	private Vector3 groundTargetPosition;
 	private float airStrikeHeight = 7.0f;
 	private Rigidbody rb;
+
+    //related to UI updates
+    public Text player1Score;
+    public Text player2Score;
+    private int player1Counter = 0;
+    private int player2Counter = 0;
 
 
 	// Use this for initialization
@@ -14,35 +21,56 @@ public class TouchUp : MonoBehaviour {
 
 		airTargetPosition = new Vector3 (0.0f, 9.0f, 9f); // x position is not relevant since it's the players x velocity
 		groundTargetPosition = new Vector3 (0.0f, 8f, 9f); // x position is not relevant since it's the players x velocity
-	}
+
+        player1Score.text = player1Counter.ToString();
+        player2Score.text = player2Counter.ToString();
+    }
 	
 	void OnCollisionExit(Collision collision){
 
 		Transform other = collision.gameObject.transform;
-		//Debug.Log (other.tag);
+        //Debug.Log (other.tag);
 
-		if (other.tag == "Player") {
-			Rigidbody otherRb = collision.gameObject.GetComponent<Rigidbody> ();
-			if (other.transform.position.y < airStrikeHeight) {
-				Vector3 assistedVelocity = (airTargetPosition - other.position).normalized * 15.0f;
-				rb.velocity = new Vector3 (rb.velocity.normalized.x*15.0f, assistedVelocity.y, assistedVelocity.z);
-			} else {
-				// Debug.Log ("Down Strike!");
-				Vector3 assistedVelocity = (groundTargetPosition - other.position).normalized * 30.0f;
-				rb.velocity = new Vector3 (rb.velocity.normalized.x*20.0f, assistedVelocity.y, assistedVelocity.z);
-			}
+        if (other.tag == "Player")
+        {
+            Rigidbody otherRb = collision.gameObject.GetComponent<Rigidbody>();
+            if (other.transform.position.y < airStrikeHeight)
+            {
+                Vector3 assistedVelocity = (airTargetPosition - other.position).normalized * 15.0f;
+                rb.velocity = new Vector3(rb.velocity.normalized.x * 15.0f, assistedVelocity.y, assistedVelocity.z);
+            }
+            else {
+                // Debug.Log ("Down Strike!");
+                Vector3 assistedVelocity = (groundTargetPosition - other.position).normalized * 30.0f;
+                rb.velocity = new Vector3(rb.velocity.normalized.x * 20.0f, assistedVelocity.y, assistedVelocity.z);
+            }
 
-			//Vector3 assistedVelocity = (groundTargetPosition - other.position) * 
-						//				(	1f + 
-						//					(other.transform.position.y/12f)*1f
-						//				);
-			//}
-			//float zPPosition = other.position.z / 8.0f;
+            //Vector3 assistedVelocity = (groundTargetPosition - other.position) * 
+            //				(	1f + 
+            //					(other.transform.position.y/12f)*1f
+            //				);
+            //}
+            //float zPPosition = other.position.z / 8.0f;
 
-			// Debug.Log ("Assist: " + new Vector3(0.0f, 9.0f, assistZ/zPPosition));
+            // Debug.Log ("Assist: " + new Vector3(0.0f, 9.0f, assistZ/zPPosition));
 
-			//otherRb.velocity  = new Vector3(otherRb.velocity.x, zPPosition*assistY, assistZ - assistZ*zPPosition);
-		}
+            //otherRb.velocity  = new Vector3(otherRb.velocity.x, zPPosition*assistY, assistZ - assistZ*zPPosition);
+        }
+        else if(other.name == "Ground")
+        {
+            if(rb.position.z > 9)
+            {
+                //increase score for player 1
+                player1Counter++;
+            }
+            else
+            {
+                //increase score for player 2
+                player2Counter++;
+            }
 
+            player1Score.text = player1Counter.ToString();
+            player2Score.text = player2Counter.ToString();
+        }
 	}
 }
